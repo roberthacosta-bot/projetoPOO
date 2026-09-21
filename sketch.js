4b22f8b46f3005ab507b5d8e85ce58a9340842c3
Robertha <robertha.costa@academico.ifpb.edu.br>
2026-09-21 14:07:27 +0000

    atualizações

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// http://10.12.131.25:5501/plataforma-aula/assets/
+// http://10.12.130.73:5501/plataforma-aula/assets/
 
 let img;
 let imgCenario;
@@ -8,22 +8,21 @@ let g;
 let largura;
 let personagem;
 let plataformas = [];
+let imgPlataformas;
 let font;
 let executando = true;
 let itens = [];
-let imgMoeda;
+let imgCoracao;
 let pontos = 0;
 
-
 async function setup() {
-  createCanvas(3000, 700);
+  createCanvas(800, 600);
 
-
-  font = await loadFont("Honk-Regular-VariableFont_MORF,SHLN.ttf");
-  img = await loadImage("flora.png");
-  imgMoeda = await loadImage("coracao.png");
-  imgCenario = await loadImage("image.png");
-  imgCenario.resize(3000, 700);
+  font = await loadFont("assets/Honk-Regular-VariableFont_MORF,SHLN.ttf");
+  img = await loadImage("assets/flora.png");
+  imgCoracao = await loadImage("assets/coracao.png");
+  imgCenario = await loadImage("assets/cenario2.png");
+  imgCenario.resize(800, 600);
   x = width / 2;
   y = 10;
   vx = 0;
@@ -31,21 +30,32 @@ async function setup() {
   g = 0.8;
   largura = 100;
 
-
   personagem = new Personagem(x, y, largura, 0, img);
 
+  imgPlataformas = {
+    madeira: await loadImage("assets/plataforma-madeira.png"),
+    pedra: await loadImage("assets/plataforma-pedra.png"),
+    terra: await loadImage("assets/plataforma-terra.png"),
+  };
 
-  plataformas.push(new Plataforma(0, height - 70, width, 70));
-  plataformas.push(new Plataforma(500, height - 210, 150, 40));
-  plataformas.push(new Plataforma(360, height - 340, 150, 40));
-  plataformas.push(new Plataforma(width + 130, height - 70, width, 70));
+  plataformas.push(
+    new Plataforma(0, height - 70, 40, 40, 15, imgPlataformas.terra),
+  );
+  plataformas.push(
+    new Plataforma(500, height - 210, 40, 40, 5, imgPlataformas.pedra),
+  );
+  plataformas.push(
+    new Plataforma(360, height - 340, 40, 40, 5, imgPlataformas.pedra),
+  );
+  plataformas.push(
+    new Plataforma(width + 130, height - 70, 40, 40, 5, imgPlataformas.terra),
+  );
 
-
-  itens.push(new Item(width + 45, height - 250, 40, 40, imgMoeda));
-  itens.push(new Item(width + 190, height - 350, 40, 40, imgMoeda));
-  itens.push(new Item(width + 316, height - 450, 40, 40, imgMoeda));
+  itens.push(new Item(width + 45, height - 250, 40, 40, imgCoracao));
+  itens.push(new Item(100, height - 140, 40, 40, imgCoracao));
+  itens.push(new Item(150, height - 140, 40, 40, imgCoracao));
+  itens.push(new Item(200, height - 140, 40, 40, imgCoracao));
 }
-
 
 function draw() {
   if (executando == true) {
@@ -54,7 +64,6 @@ function draw() {
     desenharGameOver();
   }
 }
-
 
 function desenharGameOver() {
   imgCenario.filter(GRAY);
@@ -66,41 +75,38 @@ function desenharGameOver() {
   text("Game Over", width / 2, height / 2);
 }
 
-
 function desenharPontuacao() {
   fill("#ffffff83");
   noStroke();
   rect(40, 40, 150, 60);
-  image(imgMoeda, 50, 50);
+  image(imgCoracao, 50, 50);
   fill("black");
   textSize(30);
   text("x " + pontos, 95, 80);
 }
-
 
 function desenharJogo() {
   background(220);
   image(imgCenario, 0, 0);
   desenharPontuacao();
 
-
   if (keyIsDown(LEFT_ARROW)) {
+    personagem.olharParaEsquerda();
     // Move todas as plataformas para a direita
     for (let i = 0; i < plataformas.length; i++) {
       plataformas[i].moverDireita();
     }
-
 
     // Move todos os itens para a direita
     for (let i = 0; i < itens.length; i++) {
       itens[i].moverDireita();
     }
   } else if (keyIsDown(RIGHT_ARROW)) {
+    personagem.olharParaDireita();
     // Move todas as plataformas para a esquerda
     for (let i = 0; i < plataformas.length; i++) {
       plataformas[i].moverEsquerda();
     }
-
 
     // Move todos os itens para a esquerda
     for (let i = 0; i < itens.length; i++) {
@@ -110,15 +116,12 @@ function desenharJogo() {
     personagem.parar();
   }
 
-
   // todas as atualizações
-  personagem.mover();
-
+  personagem.aplicarGravidade();
 
   if (personagem.y > height) {
     executando = false;
   }
-
 
   for (let i = 0; i < itens.length; i++) {
     if (personagem.checarColisao(itens[i])) {
@@ -127,26 +130,22 @@ function desenharJogo() {
     }
   }
 
-
   for (let plataforma of plataformas) {
     if (personagem.checarColisao(plataforma) == 1) {
       personagem.pisarNoChao(plataforma);
     } else if (personagem.checarColisao(plataforma) == 2) {
       personagem.baterCabeca(plataforma);
-      plataforma.setCor("#0a6b1a");
+      plataforma.setCor("#FF0000");
     }
   }
 
-
   // todos os dsenhos
   personagem.desenhar();
-
 
   // Desenha todas as plataformas
   for (let plataforma of plataformas) {
     plataforma.desenhar();
   }
-
 
   // Desenha todos os itens
   for (let item of itens) {
@@ -154,13 +153,8 @@ function desenharJogo() {
   }
 }
 
-
 function keyPressed() {
   if (keyCode == 38) {
     personagem.pular();
   }
 }
-
-
-
-
